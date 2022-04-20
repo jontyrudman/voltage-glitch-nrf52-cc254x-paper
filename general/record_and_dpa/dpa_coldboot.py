1cfc84a03d743804393c4179ecaa271f99e0680d
Jonathan Rudman <jonathan.rudman@live.co.uk>
2022-04-20 22:54:15 +0100

    Update with messy dpa python files

--- general/record_and_dpa/dpa_coldboot.py
+++ general/record_and_dpa/dpa_coldboot.py
@@ -16,7 +16,7 @@ matplotlib.rcParams.update({
     'pgf.rcfonts': False,
 })
 
-directories = ["traces_222403_111916_reset"]
+directories = ["traces_222603_074559_coldboot"]
 combined_file_list = [os.path.join(d, p) for d in directories for p in os.listdir(d)]
 
 crpenabled_paths = [
@@ -67,10 +67,11 @@ def align_peaks(traces):
 #         d["y"] for d in crpenabled_all_traces[:max_len]
 #     ]), axis=0),
 # }
+
 # crpdisabled_mean = {
 #     "x": crpdisabled_all_traces[0]["x"],
 #     "y": np.mean(np.array([
-#         d["y"] for d in crpdisabled_all_traces[:max_len]
+#         d["y"] for d in crpdisabled_all_traces[:max_len] if len(d["y"]) == 300000
 #     ]), axis=0),
 # }
 
@@ -80,14 +81,14 @@ def align_peaks(traces):
 #     "y": np.subtract(crpdisabled_mean["y"], crpenabled_mean["y"]) * 15,
 # }
 
-# pickle.dump(diff_trace, open("diff_trace_cached.pkl", "wb"))
-# pickle.dump(crpdisabled_all_traces[0], open("crpdisabled_cached.pkl", "wb"))
-# pickle.dump(crpenabled_all_traces[0], open("crpenabled_cached.pkl", "wb"))
+# pickle.dump(diff_trace, open("diff_trace_coldboot_cached.pkl", "wb"))
+# pickle.dump(crpdisabled_all_traces[0], open("crpdisabled_coldboot_cached.pkl", "wb"))
+# pickle.dump(crpenabled_all_traces[0], open("crpenabled_coldboot_cached.pkl", "wb"))
 # exit()
 
-diff_trace = read_path_list(["diff_trace_cached.pkl"])[0]
-crpdisabled_all_traces = read_path_list(["crpdisabled_cached.pkl"])
-crpenabled_all_traces = read_path_list(["crpenabled_cached.pkl"])
+diff_trace = read_path_list(["diff_trace_coldboot_cached.pkl"])[0]
+crpdisabled_all_traces = read_path_list(["crpdisabled_coldboot_cached.pkl"])
+crpenabled_all_traces = read_path_list(["crpenabled_coldboot_cached.pkl"])
 
 plot_trace(crpdisabled_all_traces[0]["x"] * 1000, crpdisabled_all_traces[0]["y"] + 50)
 plot_trace(crpenabled_all_traces[0]["x"] * 1000, crpenabled_all_traces[0]["y"])
@@ -101,4 +102,4 @@ plot_trace(diff_trace["x"], diff_trace["y"])
 plt.xlabel("Time (ms)")
 plt.yticks([])
 
-plt.savefig("reset_comparison.pgf")
+plt.savefig("coldboot_comparison.pgf")
